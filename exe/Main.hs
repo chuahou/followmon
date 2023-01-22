1b24f5a1649c7d3459c151b9850347dc14efca32
Chua Hou <human+github@chuahou.dev>
2023-01-22 17:42:03 +0800

    flake: init with haskell.nix

---
+++ exe/Main.hs
@@ -0,0 +1,7 @@
+-- SPDX-License-Identifier: MIT
+-- Copyright (c) 2023 Chua Hou
+
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "I'm alive!"
---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  };
+
+  # haskell.nix binary cache
+  nixConfig = {
+    extra-substituters = "https://cache.iog.io";
+    extra-trusted-public-keys = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
+  };
+
+  outputs = inputs@{ self, nixpkgs, haskellNix, ... }:
+  let
+    system = "x86_64-linux";
+    overlays = [
+      haskellNix.overlay
+      (self: super: {
+        followmon = self.haskell-nix.project' {
+          src = self.haskell-nix.haskellLib.cleanSourceWith {
+            name = "followmon-source";
+            src = ./.;
+          };
+          compiler-nix-name = "ghc925";
+        };
+      })
+    ];
+    pkgs = import nixpkgs {
+      inherit system overlays;
+      inherit (haskellNix) config;
+    };
+    flake = pkgs.followmon.flake {};
+
+  in flake // {
+    defaultPackage.${system} = flake.packages."followmon:exe:followmon";
+    devShell.${system} = pkgs.followmon.shellFor {
+      tools.hoogle = {
+        version = "5.0.18.3";
+        index-state = "2023-01-22T00:00:00Z";
+        materialized = ./materialized/hoogle;
+      };
+      nativeBuildInputs = with pkgs; [
+        cabal-install
+        hpack
+      ];
+      exactDeps = true;
+    };
+  };
+}
