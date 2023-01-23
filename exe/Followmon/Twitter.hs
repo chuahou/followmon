ceaeded9de5693a76fb1820c16c7c34673ac5c53
Chua Hou <human+github@chuahou.dev>
2023-01-23 18:16:19 +0800

    Followmon.Twitter: use followers' IDs endpoint from 1.1
    - Has a bigger count of 5000 compared to 1000, which is important due to
      only being able to make 15 requests per 15 minutes.

--- exe/Followmon/Twitter.hs
+++ exe/Followmon/Twitter.hs
@@ -7,39 +7,26 @@
 
 module Followmon.Twitter (UserID, getFollowerIDs) where
 
-import Followmon.Log             qualified as Log
+import Followmon.Log           qualified as Log
 
-import Control.Monad.Trans.Maybe (MaybeT (..))
-import Data.Aeson                (FromJSON (..), Object, Value, withObject,
-                                  (.:), (.:?))
-import Data.Aeson.Types          (Parser, listParser)
-import Data.Function             ((&))
-import Data.Set                  (Set)
-import Data.Set                  qualified as Set
-import Data.String.Interpolate   (i)
-import GHC.Generics              (Generic)
-import Network.HTTP.Simple       (addToRequestQueryString, getResponseBody,
-                                  httpJSON, setRequestBearerAuth)
+import Data.Aeson              (FromJSON (..), withObject, (.:))
+import Data.Function           ((&))
+import Data.Set                (Set)
+import Data.Set                qualified as Set
+import Data.String.Interpolate (i)
+import GHC.Generics            (Generic)
+import Network.HTTP.Simple     (addToRequestQueryString, getResponseBody,
+                                httpJSON, setRequestBearerAuth)
 
 -- | Type of user IDs.
-type UserID = String
+type UserID = Integer
 
 -- | Type of response to the followers endpoint.
 data FollowersJSON = FollowersJSON
-    { userIDs   :: [UserID]
-    , nextToken :: Maybe String -- ^ Possible pagination token.
-    } deriving Show
-instance FromJSON FollowersJSON where
-    parseJSON = withObject "FollowersJSON" $ \v -> FollowersJSON
-        <$> ((v .: "data") >>= listParser parseUser)
-        <*> parseMeta v
-            where
-                parseMeta :: Object -> Parser (Maybe String)
-                parseMeta v = runMaybeT $ do
-                        meta <- MaybeT $ v .:? "meta"
-                        MaybeT $ meta .:? "next_token"
-                parseUser :: Value -> Parser UserID
-                parseUser = withObject "userID" (.: "id")
+    { ids             :: [UserID]
+    , next_cursor_str :: Maybe String -- ^ Possible pagination token.
+    } deriving (Generic, Show)
+instance FromJSON FollowersJSON
 
 -- | JSON object wrapped in a "data" field.
 newtype DataWrapper a = DataWrapper { content :: a } deriving Show
@@ -54,46 +41,39 @@ data UserJSON = UserJSON
     } deriving (Generic, Show)
 instance FromJSON UserJSON
 
--- | JSON object containing next_token for pagination. We don't care about any
--- other fields, and we're okay with it being missing.
-newtype MetaJSON = MetaJSON { next_token :: Maybe String }
-    deriving (Generic, Show)
-instance FromJSON MetaJSON
-
--- | Twitter API v2 base URL.
+-- | Twitter API base URL.
 twitterAPI :: String
-twitterAPI = "https://api.twitter.com/2"
+twitterAPI = "https://api.twitter.com"
 
 -- | Get user IDs of followers of a username. Throws an error in IO if the
 -- request fails or a parse error occurs.
 getFollowerIDs :: String -- ^ Bearer token.
                -> String -- ^ Username to lookup.
                -> IO (Set UserID)
-getFollowerIDs tok username = do
-    Log.info [i|Looking up username #{username}|]
-    DataWrapper user <- getResponseBody <$> httpJSON
-            ([i|GET #{twitterAPI}/users/by/username/#{username}|]
-                & setRequestBearerAuth [i|#{tok}|])
-                    :: IO (DataWrapper UserJSON)
-    Log.info [i|Looked up username #{username}, got #{user}|]
-    go user.id Nothing
-        where
-            -- Look up followers of given user ID, with a possible pagination
-            -- token.
-            go :: UserID -> Maybe String -> IO (Set UserID)
-            go uid mNextTok = do
-                let req = [i|GET #{twitterAPI}/users/#{uid}/followers|]
-                        & setRequestBearerAuth [i|#{tok}|]
-                        & addToRequestQueryString [("max_results", Just "1000")]
-                let req' = maybe req (\nextTok ->
-                            req & addToRequestQueryString
-                                [("pagination_token", Just [i|#{nextTok}|])])
-                            mNextTok -- Add pagination token if necessary.
-                Log.info [i|Requesting followers of #{uid}|]
-                response <- getResponseBody <$> httpJSON req'
-                            :: IO FollowersJSON
-                let followers = Set.fromList response.userIDs
-                Log.info [i|Received #{Set.size followers} followers|]
-                case response.nextToken of
-                  Just nextTok -> Set.union followers <$> go uid (Just nextTok)
-                  Nothing      -> pure followers
+getFollowerIDs tok username = go Nothing
+    where
+        go :: Maybe String -- ^ Possible next cursor.
+           -> IO (Set UserID)
+        go mCursor = do
+            let count = 5000 -- How many to request each time.
+            let req = [i|GET #{twitterAPI}/1.1/followers/ids.json|]
+                    & setRequestBearerAuth [i|#{tok}|]
+                    & addToRequestQueryString
+                        [ ("screen_name", Just [i|#{username}|])
+                        , ("count", Just [i|#{count}|])
+                        ]
+            let req' = maybe req (\cursor ->
+                        req & addToRequestQueryString
+                            [("cursor", Just [i|#{cursor}|])])
+                        mCursor -- Add pagination token if necessary.
+            Log.info [i|Requesting followers of #{username}|]
+            response <- getResponseBody <$> httpJSON req'
+                        :: IO FollowersJSON
+            let followers = Set.fromList response.ids
+            let nReceived = Set.size followers
+            Log.info [i|Received #{nReceived} followers|]
+            -- If we have a next cursor, and we got less than expected, go to
+            -- the next page.
+            case (response.next_cursor_str, nReceived == count) of
+              (Just cursor, True) -> Set.union followers <$> go (Just cursor)
+              _                   -> pure followers
