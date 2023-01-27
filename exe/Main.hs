41b30f559ae8c1d00cab3165845f83d1eb953410
Chua Hou <human+github@chuahou.dev>
2023-01-27 21:22:50 +0800

    Main: refactor main loop
    - Separate into multiple top-level functions.
    - Only write to disk when there are updates. We no longer blindly assume
      cache is always updated.

--- exe/Main.hs
+++ exe/Main.hs
@@ -13,13 +13,13 @@ import Followmon.Twitter
 import Control.Applicative     ((<|>))
 import Control.Concurrent      (forkIO, threadDelay)
 import Control.Exception       (SomeException, handle, throw)
-import Control.Monad           (void)
+import Control.Monad           (void, when)
 import Data.Binary             (Binary (..), decodeOrFail, encodeFile)
 import Data.ByteString.Lazy    qualified as BL
 import Data.List               (intercalate)
 import Data.Map.Strict         (Map)
 import Data.Map.Strict         qualified as Map
-import Data.Maybe              (isNothing, mapMaybe)
+import Data.Maybe              (fromMaybe, isJust, isNothing, mapMaybe)
 import Data.Set                (Set)
 import Data.Set                qualified as Set
 import Data.String.Interpolate (i)
@@ -115,6 +115,7 @@ main = getArgs >>= \case
             sendMessage cfg.telegramBotToken cfg.telegramChatID
                 [i|Initialized with #{Set.size ins} #{Incoming} and #{Set.size outs} #{Outgoing}\\.|]
             loop cfg ins outs userCache -- Enter main loop.
+
         loop :: Config -> Set UserID -> Set UserID -> UserCache -> IO ()
         loop cfg ins outs userCache = do
             -- Send healthcheck if specified.
@@ -128,119 +129,144 @@ main = getArgs >>= \case
             Log.info [i|Waiting for #{interval} seconds|]
             threadDelay $ fromIntegral interval * 1_000_000
             -- Update in and out lists.
-            (ins',  insMsg)  <- update Incoming ins
-            (outs', outsMsg) <- update Outgoing outs
+            (ins',  insAdded,  insRemoved)  <- updateList cfg Incoming ins
+            (outs', outsAdded, outsRemoved) <- updateList cfg Outgoing outs
+            -- Generate reports of changes. We use the old userCache, because of
+            -- the following scenario:
+            -- * User obtained in previous cache.
+            -- * User was deleted, can no longer be looked up, and is removed
+            --   from the follower list.
+            -- * userCache contains the user, but userCache' does not.
+            -- * report needs userCache and not userCache'.
+            insReport <- report cfg userCache Incoming (Set.size ins)
+                            insAdded insRemoved
+            outsReport <- report cfg userCache Outgoing (Set.size outs)
+                            outsAdded outsRemoved
+            let mReport = insReport <> outsReport -- Nothing if nothing to report.
             -- Update user cache.
-            userCache' <- updateCache (Set.union ins' outs')
-            -- Save new values to disk.
-            Log.info "Updating file on disk"
-            encodeFile cfg.filename (ins', outs', userCache')
-            -- Check if we have any updates. If so, report the updates to
-            -- Telegram.
-            let msg = insMsg <> outsMsg
-            case msg of
-                  Nothing -> loop cfg ins' outs' userCache' -- No updates.
-                  Just msg' -> do
-                      Log.info "Sending update message"
-                      sendMessage cfg.telegramBotToken cfg.telegramChatID msg'
-                      loop cfg ins' outs' userCache'
+            mNewCache <- updateCache cfg userCache (Set.union ins' outs')
+            let userCache' = fromMaybe userCache mNewCache
+            -- Save new values to disk if there are updates.
+            when (isJust mReport || isJust mNewCache) $ do
+                Log.info "Updating file on disk"
+                encodeFile cfg.filename (ins', outs', userCache')
+            -- Report updates to Telegram if necessary.
+            maybe (pure ()) (\msg -> do
+                Log.info "Sending update message"
+                sendMessage cfg.telegramBotToken cfg.telegramChatID msg
+                loop cfg ins' outs' userCache') mReport
+            -- Next iteration.
+            loop cfg ins' outs' userCache'
+
+-- | Get updated list of the specified type. Returns the new set for that type,
+-- tupled with the sets of added and removed user IDs.
+updateList :: Config -- ^ Application configuration.
+           -> FollowerType -- ^ Type to check.
+           -> Set UserID -- ^ Previous set to compare to.
+           -> IO (Set UserID, Set UserID, Set UserID)
+updateList cfg typ old = do
+    Log.info [i|Getting updated list of #{typ}|]
+    new <- getFollowerIDs cfg.twitterBearerToken cfg.username typ
+    pure (new, new Set.\\ old, old Set.\\ new)
+
+-- | Reports changes in the specified type.
+report :: Config -- ^ Application configuration.
+       -> UserCache -- ^ User information cache.
+       -> FollowerType -- ^ Type to report.
+       -> Int -- ^ Previous size of the type's list.
+       -> Set UserID -- ^ Added user IDs.
+       -> Set UserID -- ^ Removed user IDs.
+       -> IO (Maybe String)
+report cfg userCache typ oldN added removed = do
+    let addedN   = Set.size added
+        removedN = Set.size removed
+    -- Lookup users we have to report about.
+    userMap <- lookupUsersByID cfg.twitterBearerToken . Set.toList $
+                Set.union added removed
+    -- Supplement userMap with userCache in case some of the removed users were
+    -- deleted, meaning we could no longer look them up with the API, making
+    -- them missing from userMap.
+    let userMap' = Map.union (Map.map snd userCache) userMap
+    -- Helper function to look up a user in userMap', returning a generic user
+    -- named with its user ID if both lookups fail.
+        lookupUser uid = case userMap' Map.!? uid of
+                           Just u  -> u
+                           Nothing -> UserJSON uid "unknown" [i|#{uid}|]
+        addedList   = map lookupUser . Set.toList $ added
+        removedList = map lookupUser . Set.toList $ removed
+    pure $ if addedN + removedN > 0
+              then Just $ concat
+                [ printSummary addedN removedN
+                , printUsers "+" addedList
+                , printUsers "-" removedList
+                ]
+              else Nothing
+    where
+        printSummary :: Int -> Int -> String
+        printSummary addedN removedN =
+            [i|*Changes \\(#{typ}\\)*: #{format "\\+" addedN} #{format "\\-" removedN} #{changeStr}\n#{oldN} → #{oldN + change}\n|]
+            where
+                format :: String -> Int -> String
+                format pre 0 = [i|#{pre}0|]
+                format pre n = [i|*#{pre}#{n}*|]
+                change = addedN - removedN
+                changeStr
+                  | change > 0 = [i|*↑#{change}*|]
+                  | change < 0 = [i|*↓#{0 - change}*|]
+                  | otherwise  = "*\\~0*" :: String
+        printUsers :: String -> [UserJSON] -> String
+        printUsers _ [] = ""
+        printUsers bullet users =
+              (<> "\n") . intercalate "\n"
+            . map printUser $ users
                 where
-                    -- Get updated list of the specified type, and report
-                    -- changes. Returns the new set and Just msg if there's an
-                    -- update message msg, and Nothing if there are no changes.
-                    update :: FollowerType -- Type to check.
-                           -> Set UserID -- Old set to compare to.
-                           -> IO (Set UserID, Maybe String)
-                    update typ old = do
-                        Log.info [i|Getting updated list of #{typ}|]
-                        new <- getFollowerIDs cfg.twitterBearerToken
-                                    cfg.username typ
-                        let added = new Set.\\ old
-                            addedN = Set.size added
-                            removed = old Set.\\ new
-                            removedN = Set.size removed
-                        userMap <- lookupUsersByID cfg.twitterBearerToken
-                                 . Set.toList $ Set.union added removed
-                        -- Looked-up users supplemented with user cache, in case
-                        -- some of those users were deleted since we cached them
-                        -- and hence looking them up failed.
-                        let userMap' = Map.union
-                                        (Map.map snd userCache) userMap
-                        let lookupUser m uid = case m Map.!? uid of
-                                                 Just u -> u
-                                                 Nothing -> UserJSON uid
-                                                                "unknown"
-                                                                [i|#{uid}|]
-                            added' = map (lookupUser userMap') $ Set.toList added
-                            removed' = map (lookupUser userMap') $ Set.toList removed
-                        pure $ if addedN + removedN > 0
-                            then (new, Just $ concat
-                                [ printSummary typ addedN removedN (Set.size old)
-                                , printUsers "+" added'
-                                , printUsers "-" removed'
-                                , "\n"
-                                ])
-                            else (new, Nothing)
-
-                    -- Updated user cache. Given the new set of users, it checks
-                    -- the old cache and uses those values if not expired, and
-                    -- looks them up otherwise.
-                    updateCache :: Set UserID -> IO UserCache
-                    updateCache set = do
-                        Log.info [i|Updating cache, current size #{Map.size userCache}|]
-                        t <- getCurrentTime
-                        let uids = Set.toList set
-                            maxDiffTime = fromIntegral cfg.userCacheSeconds
-                            ys = map (\uid -> do
-                                    case userCache Map.!? uid of
-                                      Just (ct, u) ->
-                                          if diffUTCTime t ct.time > maxDiffTime
-                                             then (uid, Nothing)
-                                             else (uid, Just (ct, u))
-                                      Nothing -> (uid, Nothing)) uids
-                            toRefresh = map fst . filter (isNothing . snd) $ ys
-                            cache = Map.fromList
-                                  . mapMaybe (\(uid, mu) -> (uid,) <$> mu)
-                                  $ ys
-                        Log.info [i|Updating #{length toRefresh} users|]
-                        userMap <- lookupUsersByID cfg.twitterBearerToken
-                                    toRefresh
-                        ct <- CacheTime <$> getCurrentTime
-                        let userCache' = Map.union cache (Map.map (ct,) userMap)
-                        Log.info [i|Updated cache, new size #{Map.size userCache'}|]
-                        pure userCache'
-
-                    -- Formats summary line given type, number added, number
-                    -- removed, and size of set before change.
-                    printSummary :: FollowerType -> Int -> Int -> Int -> String
-                    printSummary typ addedN removedN oldN =
-                        [i|*Changes \\(#{typ}\\)*: #{format "\\+" addedN} #{format "\\-" removedN} #{changeStr}\n#{oldN} → #{oldN + change}\n|]
-                        where
-                            format :: String -> Int -> String
-                            format pre 0 = [i|#{pre}0|]
-                            format pre n = [i|*#{pre}#{n}*|]
-                            change = addedN - removedN
-                            changeStr
-                              | change > 0 = [i|*↑#{change}*|]
-                              | change < 0 = [i|*↓#{0 - change}*|]
-                              | otherwise  = "*\\~0*" :: String
-
-                    -- Formats a user nicely given a bullet point to use and a
-                    -- user JSON object. We pad usernames to 15 characters.
-                    printUser :: String -> UserJSON -> String
-                    printUser bullet user =
+                    printUser :: UserJSON -> String
+                    printUser user =
                         let username = user.username
                             padding = replicate (15 - length username) ' '
                             name = user.name
                          in [i|\\[`#{bullet}`\\]  `@#{escapeMessagePre username}#{padding}` [#{escapeMessage name}](https://twitter.com/#{username})|]
 
-                    -- Formats list of users with bullet points, returning empty
-                    -- string if list is empty.
-                    printUsers :: String -> [UserJSON] -> String
-                    printUsers _ [] = ""
-                    printUsers bullet users =
-                        (<> "\n") . intercalate "\n"
-                      . map (printUser bullet) $ users
+-- | Update user information cache. Goes through the set of users to keep in the
+-- cache, taking a value from the old cache if available and not expired, and
+-- looking it up in the API otherwise. Returns @Just newCache@ if the cache is
+-- updated, and @Nothing@ if there are no updates.
+updateCache :: Config -- ^ Application config.
+            -> UserCache -- ^ Previous copy of cache.
+            -> Set UserID -- ^ Set of users to keep in the cache.
+            -> IO (Maybe UserCache)
+updateCache cfg userCache uids = do
+    let oldSize = Map.size userCache
+    Log.info [i|Updating cache, current size #{oldSize}|]
+    t <- getCurrentTime
+    let uids' = Set.toList uids
+        maxDiffTime = fromIntegral cfg.userCacheSeconds
+        -- Check each user ID, mapping it to a tuple of itself with @Nothing@ if
+        -- we have to look it up, and @Just v@ if we are using the cached value
+        -- @v@.
+        ys = map (\uid -> do
+                case userCache Map.!? uid of
+                  Just (ct, u) ->
+                      if diffUTCTime t ct.time > maxDiffTime
+                         then (uid, Nothing)       -- Expired.
+                         else (uid, Just (ct, u))  -- Use cached value.
+                  Nothing -> (uid, Nothing)) uids' -- Not cached.
+        -- List of user IDs to refresh.
+        toRefresh = map fst . filter (isNothing . snd) $ ys
+        -- Values to take from the old cache.
+        cache = Map.fromList . mapMaybe (\(uid, mu) -> (uid,) <$> mu) $ ys
+    case toRefresh of
+      [] -> if oldSize == Map.size cache
+               then pure Nothing -- Nothing to refresh, cache size did not
+                                 -- shrink, meaning nothing was updated.
+               else pure $ Just cache
+      _ -> do -- We have things to refresh.
+        Log.info [i|Updating #{length toRefresh} users|]
+        refreshed <- lookupUsersByID cfg.twitterBearerToken toRefresh
+        ct <- CacheTime <$> getCurrentTime
+        let userCache' = Map.union cache (Map.map (ct,) refreshed)
+        Log.info [i|Updated cache, new size #{Map.size userCache'}|]
+        pure $ Just userCache'
 
 usage :: IO ()
 usage = do
